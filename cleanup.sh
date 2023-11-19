4f0cc21afed0ab3e7e315e39aa6a9c97cd94a3a8
NetSecQuin <134654399+NetSecQuin@users.noreply.github.com>
2023-11-19 13:04:46 -0700

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,18 @@
+# Running this script will help keep your filesystem tidy 🧹 
+
+# ONLY UNCOMMENT THE FOLLOWING LINES IN THIS SCRIPT IN A DIRECTORY THAT WAS ONLY USED FOR THIS EXTRACTION
+#rm -r *
+# OR 
+#rm -r <pstfilename>
+
+rm results.txt
+rm hashes.txt
+rm uniquehashes.txt
+
+#Only required if reputationcheck.sh was run also (uncomment it if you want it)
+#rm malicioushashes.txt
+#rm cleanhashes.txt
+#rm unknownhashes.txt
+#rm maliciousfiles.txt
+#rm unknownfiles.txt
+#rm cleanfiles.txt
